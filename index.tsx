4c8f55afb8f59b7aacc6aed460b39a6eca617eb1
Azriel Nurrahman <152691782+AzrielNY@users.noreply.github.com>
2025-04-24 16:58:44 +0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+
+'use client';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './page';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<Home />);
